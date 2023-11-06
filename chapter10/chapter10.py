9bc7fa9c15fc5b63c4e5908720217e5bcb27f704
michael mackenzie <michaelmackenzie98@gmail.com>
2023-11-06 18:26:52 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- chapter10/chapter10.py
+++ chapter10/chapter10.py
@@ -20,7 +20,7 @@ def close_db_connection(exception):
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource('var/schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
